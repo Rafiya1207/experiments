02448004c2b3feed065cd7e3ce6ef075a71b60bd
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-10-10 21:33:01 +0530

    add option to decode

--- printAlphabets.js
+++ printAlphabets.js
@@ -1,38 +1,46 @@
 const ALPHABETS = 'abcdefghijklmnopqrstuvwxyz';
+const REVERSED_ALPHABETS = ALPHABETS.split('').reverse();
 
-function getAlphabet(n) {
-  return ALPHABETS[n - 1];
+function getAlphabet(n, alphabets) {
+  return alphabets[n];
 }
 
-function getAlphabetPosition(alphabet) {
-  return ALPHABETS.indexOf(alphabet) + 1;
+function getAlphabetPosition(alphabet, alphabets) {
+  return alphabets.indexOf(alphabet);
 }
 
-function encodeWord(word, code) {
+function encodeWord(word, code, alphabets) {
   let encodedWord = '';
 
   for (let index = 0; index < word.length; index++) {
-    const charPosition = getAlphabetPosition(word[index]);
+    const charPosition = getAlphabetPosition(word[index], alphabets);
     const encodedPosition = (charPosition + code) % ALPHABETS.length;
-    encodedWord += getAlphabet(encodedPosition);
+    encodedWord += getAlphabet(encodedPosition, alphabets);
   }
 
   return encodedWord;
 }
 
-function encodeSentence(sentence, code) {
+function encodeSentence(sentence, code, alphabets) {
   const words = sentence.split(' ');
   const encodedWords = [];
   
   for (let index = 0; index < words.length; index++) {
     const word = words[index];
-    encodedWords.push(encodeWord(word, code));
+    encodedWords.push(encodeWord(word, code, alphabets));
   }
 
   return encodedWords.join(' ');
 }
 
 const sentence = prompt('enter sentence: ');
-const code = parseInt(prompt('enter code'));
+const code = parseInt(prompt('enter code: '));
+const typeOfCode = parseInt(prompt('encode(0)/decode(1): '));
 
-console.log(encodeSentence(sentence, code));
+let alphabets = ALPHABETS;
+
+if (typeOfCode === 1) {
+  alphabets = REVERSED_ALPHABETS;
+}
+
+console.log(encodeSentence(sentence, code, alphabets));
