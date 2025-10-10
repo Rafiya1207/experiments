c7dbe22b8e2dbb7f35c7ab415ec32e0df256164a
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-10-10 14:50:31 +0530

    Update printAlphabets.js

--- printAlphabets.js
+++ printAlphabets.js
@@ -1,9 +1,26 @@
-function getAlphabet(n) {
-  const alphabets = 'abcdefghijklmnopqrstuvwxyz';
+const ALPHABETS = 'abcdefghijklmnopqrstuvwxyz';
 
-  return alphabets[n - 1];
+function getAlphabet(n) {
+  return ALPHABETS[n - 1];
 }
 
-const n = parseInt(prompt('enter number'));
+function getAlphabetPosition(alphabet) {
+  return ALPHABETS.indexOf(alphabet) + 1;
+}
 
-console.log(getAlphabet(n));
+function encodeWord(word, code) {
+  let encodedWord = '';
+
+  for (let index = 0; index < word.length; index++) {
+    const charPosition = getAlphabetPosition(word[index]);
+    const encodedPosition = charPosition + code;
+    encodedWord += getAlphabet(encodedPosition);
+  }
+
+  return encodedWord;
+}
+
+const word = prompt('enter word');
+const code = parseInt(prompt('enter code'));
+
+console.log(encodeWord(word, code));
