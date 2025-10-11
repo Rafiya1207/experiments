d7f8cccf25a3700893479f411cf882d123ba0318
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-10-11 11:49:52 +0530

    Update and rename generateDimensions.js to generate_dimensions.js

--- patterns/generate_dimensions.js
+++ patterns/generate_dimensions.js
@@ -1,19 +1,18 @@
-const LIMIT = 10;
-
-const dimensions = [];
-
 function generateRow(row, columns) {
   const rowOfDimensions = [];
 
   for (let column = 0; column < columns; column++) {
     rowOfDimensions.push([row, column]);
   }
-
   return rowOfDimensions;
 }
 
-for (let row = 0; row < LIMIT; row++) {
-  dimensions.push(generateRow(row, LIMIT));
-}
+function generateDimensions() {
+  const LIMIT = 10;
+  const dimensions = [];
 
-console.log(dimensions);
+  for (let row = 0; row < LIMIT; row++) {
+    dimensions.push(generateRow(row, LIMIT));
+  }
+  return dimensions;
+}
