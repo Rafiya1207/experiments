7f8b196a4af7548e13d466eafe2ff6e67f9e3a8d
Rafiya <157607173+Rafiya1207@users.noreply.github.com>
2025-11-16 15:03:04 +0530

    draw circle with sin and cos

--- patterns/circle.js
+++ patterns/circle.js
@@ -1,21 +1,24 @@
-function sqr(n) {
-	return n * n;
-}
+const equation = (r, x, y) => {
+  const sint = (y - r) / r;
+  const cost = (x - r) / r;
 
-function drawCircle(r) {
-	const diameter = r * 2;
-	const circle = [];
+  return Math.round(
+    Math.sqrt(Math.pow(cost, 2) + Math.pow(sint, 2)) * r,
+  ) + r;
+};
 
-	for (let x = 0; x <= diameter; x++) {
-		let line = '';
+const circleOf = (radius) => {
+  const diameter = radius * 2;
+  const circle = [];
 
-		for (let y = 0; y <= diameter; y++) {
-			const eq = Math.round(Math.sqrt(sqr(x - r) + sqr(y - r)));
-			line += (eq === r) ? '0 ' : '  ';
-		}
-		circle.push(line);
-	}
-	console.log(circle.join('\n'));
-}
+  for (let x = 0; x < diameter + 1; x++) {
+    for (let y = 0; y < diameter + 1; y++) {
+      const char = equation(r, x, y) === r + r ? "@ " : "  ";
+      circle.push(char);
+    }
+    circle.push("\n");
+  }
+  return circle.join("");
+};
 
-drawCircle(20);
+console.log(circleOf(3));
